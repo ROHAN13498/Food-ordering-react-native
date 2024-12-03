666facad1d9f51c673708ff3d674aee2138d622a
rohan <95855114+ROHAN13498@users.noreply.github.com>
2024-12-03 15:27:38 +0530

    listed items in order

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,43 +1,75 @@
-import { StyleSheet, Image } from 'react-native';  // Import Image component
+import { StyleSheet, Image, ScrollView } from 'react-native';  
 
-import EditScreenInfo from '@/src/components/EditScreenInfo';
-import { Text, View } from '@/src/components/Themed';
-import Colors from '@/src/constants/Colors';
-import products from '@/assets/data/products';
+import { Text, View } from '@components/Themed';
+import Colors from '@/constants/Colors';
 
-const product = products[0];
+import products from '@assets/data/products';
+
+import { Product } from "@/types";
+
+interface Props {
+  product: Product;
+}
+
+const defaultImage="https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png"
+const ProductListItem = ({ product }: Props) => {
+  return (
+    <View style={styles.productContainer}>
+      <Image source={{ uri: product.image || defaultImage }} style={styles.image} />
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>${product.price}</Text>
+      </View>
+    </View>
+  );
+};
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />  {/* Add style for Image */}
-      <Text style={styles.title}>{product.name}</Text> 
-      <Text style={styles.price}>${product.price}</Text>  {/* Apply price style */}
-    </View>
+    <ScrollView style={styles.screenContainer}>
+      {products.map((product) => (
+        <ProductListItem key={product.id} product={product} />
+      ))}
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screenContainer: {
     flex: 1,
-    padding:10,
-    borderRadius:10
+    padding: 20,
+    backgroundColor: Colors.light.background,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginVertical: 10,
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-  price: {
-    color: Colors.light.tint, 
+  productContainer: {
+    alignItems: 'center',  
+    backgroundColor: 'white',  
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 15,  
+    elevation: 3, 
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
   },
   image: {
-    width: "100%",  
-    aspectRatio: 1, 
+    width: 150, 
+    height: 150,  
+    borderRadius: 10,  
+    marginBottom: 10,  
+  },
+  textContainer: {
+    alignItems: 'center',
+    backgroundColor: Colors.light.background,  
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,  
+    color: Colors.light.text,  
+  },
+  price: {
+    fontSize: 16,
+    color: Colors.light.tint, 
+    fontWeight: '600',
   },
 });
